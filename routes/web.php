58b2850198e65f86242014bf62e49f70c9c09b55
adilovua <u_adilov@mail.ru>
2022-09-27 11:06:08 +0500

    x

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BlogController::class, 'index']);
+Route::get('post/{slug}', [BlogController::class, 'show']);
 
 
 Route::group(['prefix' => 'admin'], function () {
